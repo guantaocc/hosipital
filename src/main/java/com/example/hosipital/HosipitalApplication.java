59ad2c48cb8d8bf5f76964145da699a133cf5ae9
guantaocc <1368004939@qq.com>
2024-04-10 21:53:55 +0800

    first commit

---
+++ src/main/java/com/example/hosipital/HosipitalApplication.java
@@ -0,0 +1,13 @@
+package com.example.hosipital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HosipitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HosipitalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hosipital/HosipitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hosipital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HosipitalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
